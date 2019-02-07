a1e0f41ca4e88ad925a9c14f0212ac1c51a036c8
AntiDotStudio <antidotstudio@gmail.com>
2019-02-07 15:05:37 +0100

    Trying to continue Circular Motion

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Numerics;
+using System.Numerics.Complex;
 
 public class Player : KinematicBody
 {
@@ -22,18 +24,20 @@ public class Player : KinematicBody
 	[Export] int mapRadius = 10;
 	[Export] bool isRight = false;
     Vector3 velocity = new Vector3();
+	float dotCalculationAcos;
+	[Export] public Vector3 mapCenter = new Vector3(0, 0, 0);
 	private Vector3 tempVector = new Vector3();
 
     public void GetInput()
     {
         velocity = new Vector3();
         if (Input.IsActionPressed("ui_right")){
-            velocity.x -= mapRadius;
-			velocity.z += mapRadius;
+            velocity.x += 10;
+			velocity.z += 10;
 			isRight = true;
         }else if (Input.IsActionPressed("ui_left")){
-            velocity.x -= mapRadius;
-			velocity.z += mapRadius;
+            velocity.x -= 10;
+			velocity.z -= 10;
 			isRight = false;
         }else{
 			velocity.x = 0;
@@ -47,23 +51,26 @@ public class Player : KinematicBody
     public override void _PhysicsProcess(float delta)
     {
 		timeCounter += delta;
-		if(timeCounter >= 1){
-			timeCounter = 0;
-		}
 		velocity = new Vector3();
         GetInput();
 		velocity.y -= 5;
 		tempVector = new Vector3();
 		//MoveAndSlide(new Vector3(10*Mathf.Cos(-(2 * 3.14f * timeCounter)), 0, 10*Mathf.Sin(2 * 3.14f * timeCounter)));
-		GD.Print((velocity.x*Mathf.Acos(-1)));
-		GD.Print(this.Translation);
-		//GD.Print(Mathf.Cos(10));
+		//GD.Print(Mathf.Deg2Rad(timeCounter));
+		//GD.Print(Mathf.Abs(Mathf.Cos(timeCounter)));
+		//Complex valueComplex = new Complex(1, 1);
+		//GD.Print(this.Translation);
+		Complex c = Complex.Sqrt(-1);
+		c = Mathf.Sqrt(c);
+		GD.Print(-c*Mathf.Log(timeCounter + c*Mathf.Pow((1 - Mathf.Pow(timeCounter, 2)), 1/2)));
+		Vector3 distanceToCenter = mapCenter - this.Translation;
+		distanceToCenter = distanceToCenter.Normalized();
         if(isRight == true){
-			tempVector = new Vector3(velocity.x*Mathf.Cos(timeCounter), 0, velocity.z*Mathf.Sin(timeCounter));
-			MoveAndSlide(tempVector.Normalized());
+			tempVector = new Vector3(velocity.x*Mathf.Cos(distanceToCenter.x), 0, velocity.z*Mathf.Sin(distanceToCenter.z));
+			MoveAndSlide(tempVector);
 		}else{
-			tempVector = new Vector3(velocity.x*Mathf.Cos(timeCounter), 0, velocity.z*(-Mathf.Sin(timeCounter)));
-			MoveAndSlide(tempVector.Normalized());
+			tempVector = new Vector3(velocity.x*(-1/(Mathf.Sqrt(1-Mathf.Pow(distanceToCenter.x, 2)))), 0, velocity.z*(-1/(Mathf.Sqrt(1-Mathf.Pow(distanceToCenter.z, 2)))));
+			MoveAndSlide(tempVector);
 		}
     }
 
